a30cec5ae0127dcc1240f5fd159e06560fd0dc95
soo-essoklina-ulrich <sessoklinaulrich@gmail.com>
2025-06-27 19:13:29 +0000

    feat: add `Navbar` to `BackOfficeLayout` for improved navigation structure

--- app/(backoffice)/layout.tsx
+++ app/(backoffice)/layout.tsx
@@ -2,12 +2,20 @@ import { Metadata } from "next";
 
 import { ChildrenType } from "@/types/types";
 import AuthGuard from "@/components/nextauth/AuthGuard";
+import { Navbar } from "@/components/shared/front/navbar";
 
 export const metadata: Metadata = {
   title: "Administration",
 };
 const BackOfficeLayout = ({ children }: ChildrenType) => {
-  return <AuthGuard>{children}</AuthGuard>;
+  return (
+    <AuthGuard>
+      <div className="relative flex flex-col h-screen">
+        <Navbar />
+        {children}
+      </div>
+    </AuthGuard>
+  );
 };
 
 export default BackOfficeLayout;
