2bdf2a4e464fea7d7786aa84dd0b2b62c196628d
soo-essoklina-ulrich <sessoklinaulrich@gmail.com>
2025-06-28 22:41:32 +0000

    refactor: simplify dashboard page layout and move sidebar with breadcrumbs to private layout
    - Removed sidebar and breadcrumb functionality from `dashboard/page.tsx`.
    - Centralized sidebar and breadcrumb implementation in `private/layout.tsx` for consistency.

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -1,8 +1,47 @@
-import { ChildrenType } from "@/types/types";
+import {ChildrenType} from "@/types/types";
 import AuthGuard from "@/components/auth/AuthGuard";
+import {SidebarInset, SidebarProvider, SidebarTrigger} from "@/components/ui/sidebar";
+import {AppSidebar} from "@/components/app-sidebar";
+import {Separator} from "@/components/ui/separator";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator
+} from "@/components/ui/breadcrumb";
 
 const PrivateLayout = ({ children }: ChildrenType) => {
-  return <AuthGuard>{children}</AuthGuard>;
+  return( <AuthGuard><SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+            <div className="flex items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
+              <Separator
+                  className="mr-2 data-[orientation=vertical]:h-4"
+                  orientation="vertical"
+              />
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem className="hidden md:block">
+                    <BreadcrumbLink href="#">
+                      Building Your Application
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator className="hidden md:block" />
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+            </div>
+          </header>
+          {children}
+        </SidebarInset>
+      </SidebarProvider>
+ </AuthGuard>);
 };
 
 export default PrivateLayout;
