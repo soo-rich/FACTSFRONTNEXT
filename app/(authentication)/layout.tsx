7137f28d9ac1132552e8e0fbddd15530c503a363
soo-essoklina-ulrich <sessoklinaulrich@gmail.com>
2025-06-30 16:33:52 +0000

    feat: add AuthenticationLayout and SignInPage components for user authentication flow

---
+++ app/(authentication)/layout.tsx
@@ -0,0 +1,30 @@
+import { ChildrenType } from '@/types/types';
+
+const AuthenticationLayout = ({ children }: ChildrenType) => {
+    return (
+        <div className="relative p-6 bg-white z-1 dark:bg-gray-900 sm:p-0">
+            <ThemeProvider>
+                <div className="relative flex lg:flex-row w-full h-screen justify-center flex-col  dark:bg-gray-900 sm:p-0">
+                    {children}
+                    <div className="lg:w-1/2 w-full h-full bg-brand-950 dark:bg-white/5 lg:grid items-center hidden">
+                        <div className="relative items-center justify-center  flex z-1">
+                            {/* <!-- ===== Common Grid Shape Start ===== --> */}
+                            <GridShape />
+                            <div className="flex flex-col items-center max-w-xs">
+                                <Link href="/" className="block mb-4">
+                                    <Image width={231} height={48} src="/logo-rbg.png" alt="Logo" />
+                                </Link>
+                                <p className="text-center text-gray-400 dark:text-white/60">Connecter a l'espace administrateur .</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="fixed bottom-6 right-6 z-50 hidden sm:block">
+                        <ThemeTogglerTwo />
+                    </div>
+                </div>
+            </ThemeProvider>
+        </div>
+    );
+};
+
+export default AuthenticationLayout;
