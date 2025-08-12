cb32bbf409fd8c361f85dc6b426d5d342c5ec70c
Sul <sessoklinaulrich@gmail.com>
2025-08-12 15:59:05 +0000

    feat: add rich text editor for article description and update article schema

--- src/types/soosmart/article.type.ts
+++ src/types/soosmart/article.type.ts
@@ -4,11 +4,13 @@ import { minLength, number, object, pipe, string } from 'valibot'
 export type ArticleType = {
   id: string;
   libelle: string;
+  description: string;
   prix_unitaire: number;
 };
 
 export const articleSchema = object({
   libelle: pipe(string(), minLength(1, 'le libelle est requis')),
+  description: string(),
   prix_unitaire: number()
 })
 export type SaveArticleType = InferInput<typeof articleSchema>;
