13ab3f665574c02cad304854a7b8e108df9b089e
soo-essoklina-ulrich <sessoklinaulrich@gmail.com>
2025-11-27 18:48:25 +0000

    refactor(SecurityTab): comment out unused component imports and related JSX

--- src/views/soosmart/profile/user-right/security/index.tsx
+++ src/views/soosmart/profile/user-right/security/index.tsx
@@ -3,8 +3,9 @@ import Grid from '@mui/material/Grid2'
 
 // Component Imports
 import ChangePassword from './ChangePassword'
-import TwoStepVerification from './TwoStepVerification'
-import RecentDevice from './RecentDevice'
+
+// import TwoStepVerification from './TwoStepVerification'
+// import RecentDevice from './RecentDevice'
 
 const SecurityTab = () => {
   return (
@@ -12,12 +13,12 @@ const SecurityTab = () => {
       <Grid size={{ xs: 12 }}>
         <ChangePassword />
       </Grid>
-      <Grid size={{ xs: 12 }}>
+      {/* <Grid size={{ xs: 12 }}>
         <TwoStepVerification />
       </Grid>
       <Grid size={{ xs: 12 }}>
         <RecentDevice />
-      </Grid>
+      </Grid> */}
     </Grid>
   )
 }
