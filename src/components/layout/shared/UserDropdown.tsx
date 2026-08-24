2574a25824e7e4aad5f1ab4203f3cb5f3567cf32
soo-rich <sessoklinaulrich@gmail.com>
2026-08-24 00:53:53 +0000

    fix(dropdown): update profile link and remove unused menu items

--- src/components/layout/shared/UserDropdown.tsx
+++ src/components/layout/shared/UserDropdown.tsx
@@ -134,23 +134,10 @@ const UserDropdown = () => {
                     </div>
                   </div>
                   <Divider className='mlb-1' />
-                  <MenuItem className='mli-2 gap-3' onClick={e => handleDropdownClose(e, '/pages/user-profile')}>
+                  <MenuItem className='mli-2 gap-3' onClick={e => handleDropdownClose(e, '/profile')}>
                     <i className='tabler-user' />
                     <Typography color='text.primary'>My Profile</Typography>
                   </MenuItem>
-                  <MenuItem className='mli-2 gap-3' onClick={e => handleDropdownClose(e, '/pages/account-settings')}>
-                    <i className='tabler-settings' />
-                    <Typography color='text.primary'>Settings</Typography>
-                  </MenuItem>
-                  <MenuItem className='mli-2 gap-3' onClick={e => handleDropdownClose(e, '/pages/pricing')}>
-                    <i className='tabler-currency-dollar' />
-                    <Typography color='text.primary'>Pricing</Typography>
-                  </MenuItem>
-                  <MenuItem className='mli-2 gap-3' onClick={e => handleDropdownClose(e, '/pages/faq')}>
-                    <i className='tabler-help-circle' />
-                    <Typography color='text.primary'>FAQ</Typography>
-                  </MenuItem>
-                  <Divider className='mlb-1' />
                   <MenuItem
                     className='mli-2 gap-3'
                     onClick={() => {
--- src/components/layout/vertical/NavbarContent.tsx
+++ src/components/layout/vertical/NavbarContent.tsx
@@ -1,11 +1,10 @@
 // Third-party Imports
 import classnames from 'classnames'
 
-
-
 // Component Imports
 import NavToggle from './NavToggle'
-import NavSearch from '@components/layout/shared/search'
+
+// import NavSearch from '@components/layout/shared/search'
 import LanguageDropdown from '@components/layout/shared/LanguageDropdown'
 import ModeDropdown from '@components/layout/shared/ModeDropdown'
 
@@ -14,15 +13,12 @@ import UserDropdown from '@components/layout/shared/UserDropdown'
 // Util Imports
 import { verticalLayoutClasses } from '@layouts/utils/layoutClasses'
 
-
 const NavbarContent = () => {
   return (
     <div className={classnames(verticalLayoutClasses.navbarContent, 'flex items-center justify-between gap-4 is-full')}>
       <div className='flex items-center gap-4'>
         <NavToggle />
-        <span id='tour-search'>
-          <NavSearch />
-        </span>
+        <span id='tour-search'>{/* <NavSearch /> */}</span>
       </div>
       <div className='flex items-center'>
         <LanguageDropdown />
